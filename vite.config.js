20ec5a20dd62eb3611e936c6a2680d48948eea7a
Lamalice13 <139547486+Lamalice13@users.noreply.github.com>
2026-02-27 16:44:46 +0100

    Build: add base config for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
+  base: "/ronin-portfolio/",
   plugins: [react(), tailwindcss(), svgr()],
   resolve: {
     alias: {
