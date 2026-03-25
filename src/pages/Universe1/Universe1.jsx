2e8faf2e2b63741273d82d1dc0766a9446c1860b
Lamalice13 <139547486+Lamalice13@users.noreply.github.com>
2026-03-25 17:52:25 +0100

    fix: fix blackhole dimensions

--- src/pages/Universe1/Universe1.jsx
+++ src/pages/Universe1/Universe1.jsx
@@ -2,9 +2,17 @@ import blackHole from "../../assets/videos/0001-0250.webm";
 
 export default function Universe1() {
   return (
-    <video autoPlay loop muted playsInline>
-      <source src={blackHole} type='video/webm' />
-      Votre navigateur ne supporte pas la vidéo.
-    </video>
+    <div className='w-screen h-screen bg-amber-300'>
+      <video
+        autoPlay
+        loop
+        muted
+        playsInline
+        className='w-full h-full object-cover'
+      >
+        <source src={blackHole} type='video/webm' />
+        Votre navigateur ne supporte pas la vidéo.
+      </video>
+    </div>
   );
 }
